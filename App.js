2dc08df814e01cf66fe55a50f78511493c93af6c
HarshitArora07 <162539536+HarshitArora07@users.noreply.github.com>
2024-11-20 23:10:48 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+// src/App.js
+import React, { useState } from 'react';
+import UserInputForm from './components/UserInputForm';
+import UserList from './components/UserList';
+import './App.css';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const handleUserSubmit = (user) => {
+    setUsers([...users, user]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Profile Card Generator</h1>
+      <UserInputForm onSubmit={handleUserSubmit} />
+      <UserList users={users} />
+    </div>
+  );
+};
+
+export default App;
