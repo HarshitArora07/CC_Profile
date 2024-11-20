25d2c52f6e1a2bdae5e85fc4f37e4cae00ea0191
HarshitArora07 <162539536+HarshitArora07@users.noreply.github.com>
2024-11-20 23:10:29 +0530

    Create UserList.js

---
+++ UserList.js
@@ -0,0 +1,19 @@
+// src/components/UserList.js
+import React from 'react';
+import ProfileCard from './ProfileCard';
+
+const UserList = ({ users }) => {
+  return (
+    <div className="user-list">
+      {users.length === 0 ? (
+        <p>No profiles to display</p>
+      ) : (
+        users.map((user, index) => (
+          <ProfileCard key={index} {...user} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default UserList;
